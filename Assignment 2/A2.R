ce924c20aa04ec0a640d7381d5b27686ec454f8e
RioSamson <76279598+RioSamson@users.noreply.github.com>
2023-10-18 00:50:05 -0700

    seperated the weeks of the week for question 2

--- Assignment 2/A2.R
+++ Assignment 2/A2.R
@@ -1,3 +1,24 @@
 setwd("C:/Users/User/Downloads")
 dataset <- read.table("Group_Assignment_1_Dataset.txt", header=TRUE, sep=",")
 
+#converting time and date from a string to Posixct objects for calculations
+dataset$Time <- as.POSIXlt(dataset$Time, format="%H:%M:%S")
+dataset$Date <- as.POSIXlt(dataset$Date, format = "%d/%m/%Y")
+dataset_week12 <- subset(dataset, dataset$Date >=  as.POSIXlt("19/3/2007", format="%d/%m/%Y") & dataset$Date <= as.POSIXlt("25/3/2007", format="%d/%m/%Y"))
+
+#making duplicate dataset for calculations
+dataset2 <- dataset
+dataset2$Date <- as.Date(dataset$Date, format = "%Y-%m-%d")
+
+library(lubridate)
+library(dplyr)
+library(magrittr)
+library(ISOweek)
+
+#spliting the the dataset into different weeks
+#January 1, 2007 is a monday
+#make dataset2 have an extra filed that has the week number
+#2007 starts with a monday, has 52 full weeks, plus december 31 (week 53)
+dataset2 <- dataset2 %>% mutate(Week = week(Date))
+grouped_data <- dataset2 %>% group_by(Week)
+
